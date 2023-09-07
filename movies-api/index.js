cd612b6d2827edb2bbf674e33956a1d712d6f753
Jun-Shuo Ng <20069422@mail.wit.ie>
2023-09-07 12:32:42 +0800

    "Mongo lab Users Router"

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import genresRouter from './api/genres';
+import usersRouter from './api/users';
 import './db';
 import './seedData';
 
@@ -14,6 +15,8 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use('/api/movies', moviesRouter);
 app.use('/api/genres', genresRouter);
+//Users router
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
